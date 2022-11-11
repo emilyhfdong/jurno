e923ada7f095e5c7f2b822b74b3d37a6cdf3ad66
Emily Dong <emilyhfdong@gmail.com>
2022-11-11 14:23:07 -0500

    chore: install rebass, set up theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,20 @@ import type { AppProps } from "next/app"
 import { SessionContextProvider } from "@supabase/auth-helpers-react"
 import { useState } from "react"
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs"
+import { ThemeProvider } from "@emotion/react"
+import theme from "@rebass/preset"
 
 export default function App({ Component, pageProps }: AppProps) {
   const [supabaseClient] = useState(() => createBrowserSupabaseClient())
 
   return (
-    <SessionContextProvider
-      supabaseClient={supabaseClient}
-      initialSession={pageProps.initialSession}
-    >
-      <Component {...pageProps} />
-    </SessionContextProvider>
+    <ThemeProvider theme={theme}>
+      <SessionContextProvider
+        supabaseClient={supabaseClient}
+        initialSession={pageProps.initialSession}
+      >
+        <Component {...pageProps} />
+      </SessionContextProvider>
+    </ThemeProvider>
   )
 }
---
+++ theme/index.ts
@@ -0,0 +1,7 @@
+export const theme = {
+  colors: {
+    beige: "#ECE8DC",
+    black: "#2F2E31",
+    white: "#FDFAF0",
+  },
+}
