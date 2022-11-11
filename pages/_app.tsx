07014de2af2e8428b4f42d0cdd5d98d14deb8198
Emily Dong <emilyhfdong@gmail.com>
2022-11-11 11:27:29 -0500

    chore: setup supabase client/auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import { SessionContextProvider } from "@supabase/auth-helpers-react"
+import { useState } from "react"
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient())
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  )
 }
