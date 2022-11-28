532a5240ebb0cf32bfa3d75cd7ebf18d6014c67a
Emily Dong <emilyhfdong@gmail.com>
2022-11-27 22:41:05 -0500

    feat: editor toolbar

--- components/HomeScreen/Editor/Editor.tsx
+++ components/HomeScreen/Editor/Editor.tsx
@@ -2,39 +2,17 @@ import { DateTime } from "luxon"
 import React, { useCallback, useState } from "react"
 import { useEditor, EditorContent } from "@tiptap/react"
 import StarterKit from "@tiptap/starter-kit"
+
 import { Entry } from "../../types"
 import { DATE_FORMAT } from "../constants"
 import { trpc } from "../../../utils/trpc"
 import { useAppSelector } from "../../../redux/hooks"
+import { Toolbar } from "./Toolbar"
+import { debounce, getStringifiedEntry } from "./utils"
 
 type EditorProps = {
   initialEntry: Entry
 }
-
-let saveTimer: number | ReturnType<typeof setTimeout>
-const DEBOUNCE_TIME_IN_MS = 1000
-
-const debounce = (callback: () => void) => {
-  if (saveTimer) {
-    clearTimeout(saveTimer)
-  }
-  saveTimer = setTimeout(callback, DEBOUNCE_TIME_IN_MS)
-}
-
-const INITIAL_EDITOR_JSON = {
-  type: "doc",
-  content: [
-    {
-      type: "paragraph",
-    },
-  ],
-}
-
-const getStringifiedEntry = ({
-  content,
-  title,
-}: Pick<Entry, "content" | "title">) =>
-  JSON.stringify(content ?? INITIAL_EDITOR_JSON) + title
 
 export const Editor: React.FC<EditorProps> = ({ initialEntry }) => {
   const [persistedStringifiedContent, setPersistedStringifiedContent] =
@@ -58,7 +36,7 @@ export const Editor: React.FC<EditorProps> = ({ initialEntry }) => {
     [setTitle, initialEntry, mutate]
   )
 
-  const contentEditor = useEditor({
+  const editor = useEditor({
     extensions: [StarterKit],
     onUpdate: ({ editor }) => {
       debounce(() => mutate({ content: editor.getJSON(), id: initialEntry.id }))
@@ -70,25 +48,32 @@ export const Editor: React.FC<EditorProps> = ({ initialEntry }) => {
   const isSaved =
     persistedStringifiedContent ===
     getStringifiedEntry({
-      content: contentEditor?.getJSON() ?? null,
+      content: editor?.getJSON() ?? null,
       title,
     })
 
   return (
-    <div className="flex flex-1 h-full flex-col p-4 border border-black">
-      <div className="flex justify-between items-center mb-2 text-grey">
-        <p className="text-xs">{startDate.toFormat(DATE_FORMAT)}</p>
-        <i className={`ri-check-line ${isSaved ? "" : "text-transparent"}`}></i>
-      </div>
-      <div className={`font-serif text-2xl mb-4 ${isBlurred ? "blur-sm" : ""}`}>
-        <input
-          className="bg-transparent outline-none w-full"
-          value={title || ""}
-          onChange={(e) => onTitleChange(e.target.value)}
-        />
-      </div>
-      <div className={`h-full [&_div]:h-full ${isBlurred ? "blur-sm" : ""}`}>
-        <EditorContent editor={contentEditor} />
+    <div className="flex flex-1 h-full flex-col border border-black">
+      <Toolbar editor={editor} />
+      <div className="flex flex-1 flex-col p-4">
+        <div className="flex justify-between items-center mb-2 text-grey">
+          <p className="text-xs">{startDate.toFormat(DATE_FORMAT)}</p>
+          <i
+            className={`ri-check-line ${isSaved ? "" : "text-transparent"}`}
+          ></i>
+        </div>
+        <div
+          className={`font-serif text-2xl mb-4 ${isBlurred ? "blur-sm" : ""}`}
+        >
+          <input
+            className="bg-transparent outline-none w-full"
+            value={title || ""}
+            onChange={(e) => onTitleChange(e.target.value)}
+          />
+        </div>
+        <div className={`h-full [&_div]:h-full ${isBlurred ? "blur-sm" : ""}`}>
+          <EditorContent editor={editor} />
+        </div>
       </div>
     </div>
   )
