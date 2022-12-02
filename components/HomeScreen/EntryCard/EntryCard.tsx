39a05e7fd4e4d5f6c22d5868a353c12f245aa04e
Emily Dong <emilyhfdong@gmail.com>
2022-12-02 13:55:32 -0500

    chore: split EntryCard to its own file

---
+++ components/HomeScreen/EntryCard/EntryCard.tsx
@@ -0,0 +1,68 @@
+import { EditorContent, useEditor } from "@tiptap/react"
+import StarterKit from "@tiptap/starter-kit"
+import React, { useRef } from "react"
+import { Entry } from "../../types"
+import { motion, useScroll, useTransform } from "framer-motion"
+import { DateTime } from "luxon"
+
+type EntryCardProps = {
+  entry: Entry
+}
+export const EntryCard: React.FC<EntryCardProps> = ({
+  entry: { content, createdAt, title },
+}) => {
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content,
+    editable: false,
+  })
+  const ref = useRef(null)
+  const { scrollYProgress } = useScroll({ target: ref })
+  const y = useTransform(scrollYProgress, [0, 1], [-400, 400])
+
+  const createdAtDatetime = DateTime.fromISO(createdAt)
+
+  const backgroundPositionY = useTransform(
+    scrollYProgress,
+    [0, 1],
+    ["26rem", "-26rem"]
+  )
+
+  return (
+    <section className="flex h-screen items-center snap-center justify-between pl-32 pr-8">
+      <motion.div
+        style={{ y }}
+        className="justify-center items-center flex flex-1"
+      >
+        <div className="flex items-start">
+          <div className="text-xl pt-4 font-light tracking-widest">
+            {createdAtDatetime.month}/
+          </div>
+          <motion.div
+            style={{
+              WebkitTextFillColor: "transparent",
+              backgroundImage: "linear-gradient(black, black)",
+              backgroundSize: "100% 13rem",
+              WebkitBackgroundClip: "text",
+              backgroundPositionY,
+              backgroundRepeat: "no-repeat",
+              WebkitTextStroke: "1px black",
+            }}
+            className="text-[13rem] leading-none font-bold"
+          >
+            {createdAtDatetime.day}
+          </motion.div>
+        </div>
+      </motion.div>
+      <div ref={ref}></div>
+      <div className="pt-32 pb-8 w-[65%] h-full  py-8 ">
+        <div className="flex border-b-4 h-full border-black pt-4">
+          <div className="w-[30%] top-4 font-bold text-3xl pr-8 ">{title}</div>
+          <div className="flex flex-1  text-base font-light">
+            <EditorContent editor={editor} />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
--- components/HomeScreen/HomeScreen.tsx
+++ components/HomeScreen/HomeScreen.tsx
@@ -1,14 +1,8 @@
-import { EditorContent, useEditor } from "@tiptap/react"
-import StarterKit from "@tiptap/starter-kit"
-import React, { useRef } from "react"
+import React from "react"
 import { trpc } from "../../utils/trpc"
-import { Entry } from "../types"
-import { motion, useScroll, useTransform } from "framer-motion"
-import { DateTime } from "luxon"
+import { EntryCard } from "./EntryCard"
 
-type HomeScreenProps = {}
-
-export const HomeScreen: React.FC<HomeScreenProps> = () => {
+export const HomeScreen: React.FC = () => {
   const { data } = trpc.allEntries.useQuery()
 
   return (
@@ -19,67 +13,5 @@ export const HomeScreen: React.FC<HomeScreenProps> = () => {
         <EntryCard key={entry.id} entry={entry} />
       ))}
     </div>
-  )
-}
-
-type EntryCardProps = {
-  entry: Entry
-}
-
-export const EntryCard: React.FC<EntryCardProps> = ({ entry }) => {
-  const { content, createdAt, id, title, finishedAt } = entry
-  const editor = useEditor({
-    extensions: [StarterKit],
-    content,
-    editable: false,
-  })
-  const ref = useRef(null)
-  const { scrollYProgress } = useScroll({ target: ref })
-  const y = useTransform(scrollYProgress, [0, 1], [-400, 400])
-
-  const createdAtDatetime = DateTime.fromISO(createdAt)
-
-  const backgroundPositionY = useTransform(
-    scrollYProgress,
-    [0, 1],
-    ["26rem", "-26rem"]
-  )
-
-  return (
-    <section className="flex h-screen items-center snap-center justify-between pl-32 pr-8">
-      <motion.div
-        style={{ y }}
-        className="justify-center items-center flex flex-1"
-      >
-        <div className="flex items-start">
-          <div className="text-xl pt-4 font-light tracking-widest">
-            {createdAtDatetime.month}/
-          </div>
-          <motion.div
-            style={{
-              WebkitTextFillColor: "transparent",
-              backgroundImage: "linear-gradient(black, black)",
-              backgroundSize: "100% 13rem",
-              WebkitBackgroundClip: "text",
-              backgroundPositionY,
-              backgroundRepeat: "no-repeat",
-              WebkitTextStroke: "1px black",
-            }}
-            className="text-[13rem] leading-none font-bold"
-          >
-            {createdAtDatetime.day}
-          </motion.div>
-        </div>
-      </motion.div>
-      <div ref={ref}></div>
-      <div className="pt-32 pb-8 w-[65%] h-full  py-8 ">
-        <div className="flex border-b-4 h-full border-black pt-4">
-          <div className="w-[30%] top-4 font-bold text-3xl pr-8 ">{title}</div>
-          <div className="flex flex-1  text-base font-light">
-            <EditorContent editor={editor} />
-          </div>
-        </div>
-      </div>
-    </section>
   )
 }
