ee660addbd103fee8fdf92eb9d69043ebf93bcc3
Emily Dong <emilyhfdong@gmail.com>
2022-12-02 14:16:45 -0500

    feat: handle overflowing entry content

--- components/HomeScreen/EntryCard/EntryCard.tsx
+++ components/HomeScreen/EntryCard/EntryCard.tsx
@@ -22,12 +22,18 @@ export const EntryCard: React.FC<EntryCardProps> = ({
   return (
     <section className="flex h-screen items-center snap-center justify-between pl-32 pr-8">
       <AnimatedDate date={createdAt} scrollYProgress={scrollYProgress} />
-      <div ref={ref}></div>
-      <div className="pt-32 pb-8 w-[65%] h-full  py-8 ">
-        <div className="flex border-b-4 h-full border-black pt-4">
-          <div className="w-[30%] top-4 font-bold text-3xl pr-8 ">{title}</div>
-          <div className="flex flex-1  text-base font-light">
-            <EditorContent editor={editor} />
+      <div ref={ref}>{/* dummy div to calculate scroll position */}</div>
+      <div className="pt-32 pb-8 w-[70%] h-full py-8 ">
+        <div className="flex border-b-4 h-full border-black p-6">
+          <div className="w-[35%] top-4 font-bold text-3xl mr-12 ">{title}</div>
+          <div className="flex flex-col justify-between w-full  ">
+            <div className="flex flex-1 w-full  text-base font-light overflow-scroll relative">
+              <EditorContent editor={editor} />
+              <div className="absolute bottom-0 w-full h-6 bg-gradient-to-t from-white to-transparent" />
+            </div>
+            <div className="font-bold flex items-center cursor-pointer mt-4">
+              Edit<i className="ri-arrow-right-s-line"></i>
+            </div>
           </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
         serif: ["DM Serif Text", "serif"],
       },
       colors: {
-        black: "#0D0E17",
+        black: "#1D1D1D",
         white: "#E8E8E8",
         grey: "#9F9C94",
         red: "red",
