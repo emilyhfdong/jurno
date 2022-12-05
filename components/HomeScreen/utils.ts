83d7d3ebf3114bebae5c037e12f52ebf35d8430e
Emily Dong <emilyhfdong@gmail.com>
2022-12-05 11:07:52 -0500

    feat: display entry duration

--- components/HomeScreen/utils.ts
+++ components/HomeScreen/utils.ts
@@ -9,7 +9,22 @@ export const getEntryStartEndTime = ({
 }) => {
   const createdAtDatetime = DateTime.fromISO(createdAt)
   const finishedAtDatetime = finishedAt ? DateTime.fromISO(finishedAt) : null
+
+  const durationMinutes = Math.floor(
+    finishedAtDatetime?.diff(createdAtDatetime).as("minutes") || 0
+  )
+
+  const formattedDuration = durationMinutes
+    ? durationMinutes < 60
+      ? `${durationMinutes}m`
+      : `${Math.floor(durationMinutes / 60)}h ${Math.floor(
+          durationMinutes % 60
+        )}m`
+    : ""
+
   return `${createdAtDatetime.toFormat("h:mma")}${
-    finishedAtDatetime ? ` - ${finishedAtDatetime.toFormat("h:mma")}` : ""
+    finishedAtDatetime
+      ? ` - ${finishedAtDatetime.toFormat("h:mma")} [${formattedDuration}]`
+      : ""
   }`
 }
